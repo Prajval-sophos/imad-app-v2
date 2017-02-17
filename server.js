4575beba440f354a38fa7cf96f2709cadc06c1c5
Prajval-sophos <prajval1089@gmail.com>
2017-02-17 17:29:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req,res){
-  res.send('Article one requested and will be served here');  
+app.get('/article-one', function (req, res) {
+  res.send('Article one requested and will be served here');
 });
 
-app.get('/article-two', function(req,res){
+app.get('/article-two', function (req, res) {
   res.send('Article two requested and will be served here');  
 });
 
